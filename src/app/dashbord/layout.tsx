55f4309ebdf970a940827d94b8d2211836fe820e
hardsheth <90276822+hardsheth@users.noreply.github.com>
2025-12-21 19:58:46 +0530

    React-router-dom is used

--- src/app/dashbord/layout.tsx
+++ src/app/dashbord/layout.tsx
@@ -1,15 +1,27 @@
+import Breadcrumbs from "@/components/Breadcrumbs";
+import DrawerContent from "@/components/Drawer/DrawerContent";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import NavigationScroll from "@/components/NavigationScroll";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="dashboard">
-      <Sidebar />
-      <div className="dashboard__content">
-        <Header />
-        <main className="dashboard__main">{children}</main>
+      <div>
+      <DrawerContent />
+      <Header />
+      <div className="pc-container">
+        <div className="pc-content">
+          <Breadcrumbs />
+          <NavigationScroll>
+            {children}
+          </NavigationScroll>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
